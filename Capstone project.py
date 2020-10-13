c97ffba5f28dfdf33bbdb7733d098bcdcff9ad8e
Sferrara93 <64146487+Sferrara93@users.noreply.github.com>
2020-10-13 16:40:16 +0200

    Add files via upload

---
+++ Capstone project.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Capstone Project
+
+# ###### This notebook will be used for a capstone project
+
+# In[2]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[3]:
+
+
+print('Hello Capstone Project Course!')
+
